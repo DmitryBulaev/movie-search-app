8c122cb80cdadd782ecd3c60e21f5dd124d3ea81
Dmitry Bulaev <bulaevd74@mail.ru>
2024-11-02 16:32:59 +0500

    refactoring

--- script.js
+++ script.js
@@ -45,32 +45,32 @@ function addFilmsFromApi(userReauest) {
 }
 
 filmListNode.addEventListener("click", (event) => {
-  const movieElement = event.target.closest("[data-class=filmItem]");
-  if (event.target.dataset.class !== "filmItem") return;
+  const movieElement = event.target;
+  if (movieElement.matches(".film-item")) {
+    const id = movieElement.id;
 
-  const id = movieElement.id;
-
-  fetch(`http://www.omdbapi.com/?i=${id}&apikey=218d497b`)
-    .then((response) => response.json())
-    .then((data) => {
-      wrapperNode.classList.toggle("display-none");
-      filmPageNode.classList.toggle("display-none");
-      filmPageNode.innerHTML = `<button data-class='backButton' class='film-page__back-button'>← Back to list</button>
-                                <img class='film-page__poster' src=${data.Poster} alt='Постер фильма' />
-                                <div class='profile-wrapper'>
-                                  <h2 class='film-page__title'>${data.Title}</h2>
-                                  <ul class="film-page__profile">
-                                    <li class='film-page__profile-item'>Год: <span class='film-page__profile-item_blue'>${data.Year}</span></li>
-                                    <li class='film-page__profile-item'>Рейтинг: <span class='film-page__profile-item_blue'>${data.Rated}</span></li>
-                                    <li class='film-page__profile-item'>Дата выхода: <span class='film-page__profile-item_blue'>${data.Released}</span></li>
-                                    <li class='film-page__profile-item'>Продолжительность:<span class='film-page__profile-item_blue'>${data.Runtime}</span></li>
-                                    <li class='film-page__profile-item'>Жанр: <span class='film-page__profile-item_blue'>${data.Genre}</span></li>
-                                    <li class='film-page__profile-item'>Режиссёр: <span class='film-page__profile-item_blue'>${data.Director}</span></li>
-                                    <li class='film-page__profile-item'>Сценарий: <span class='film-page__profile-item_blue'>${data.Writer}</span></li>
-                                    <li class='film-page__profile-item'>Актёры: <span class='film-page__profile-item_blue'>${data.Actors}</span></li>
-                                  </ul>
-                                </div>`;
-    });
+    fetch(`http://www.omdbapi.com/?i=${id}&apikey=218d497b`)
+      .then((response) => response.json())
+      .then((data) => {
+        wrapperNode.classList.toggle("display-none");
+        filmPageNode.classList.toggle("display-none");
+        filmPageNode.innerHTML = `<button data-class='backButton' class='film-page__back-button'>← Back to list</button>
+                                  <img class='film-page__poster' src=${data.Poster} alt='Постер фильма' />
+                                  <div class='profile-wrapper'>
+                                    <h2 class='film-page__title'>${data.Title}</h2>
+                                    <ul class="film-page__profile">
+                                      <li class='film-page__profile-item'>Год: <span class='film-page__profile-item_blue'>${data.Year}</span></li>
+                                      <li class='film-page__profile-item'>Рейтинг: <span class='film-page__profile-item_blue'>${data.Rated}</span></li>
+                                      <li class='film-page__profile-item'>Дата выхода: <span class='film-page__profile-item_blue'>${data.Released}</span></li>
+                                      <li class='film-page__profile-item'>Продолжительность:<span class='film-page__profile-item_blue'>${data.Runtime}</span></li>
+                                      <li class='film-page__profile-item'>Жанр: <span class='film-page__profile-item_blue'>${data.Genre}</span></li>
+                                      <li class='film-page__profile-item'>Режиссёр: <span class='film-page__profile-item_blue'>${data.Director}</span></li>
+                                      <li class='film-page__profile-item'>Сценарий: <span class='film-page__profile-item_blue'>${data.Writer}</span></li>
+                                      <li class='film-page__profile-item'>Актёры: <span class='film-page__profile-item_blue'>${data.Actors}</span></li>
+                                    </ul>
+                                  </div>`;
+      });
+  }
 });
 
 filmPageNode.addEventListener("click", (event) => {
